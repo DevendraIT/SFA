2375ea60828e6fe495d201a10fd403d106605a92
jainayushi-16 <jainayushi382@gmail.com>
2026-07-10 14:15:07 +0530

    Initial frontend commit

---
+++ src/api/authApi.js
@@ -0,0 +1,18 @@
+import api from "../services/api";
+
+export const login = (data) => {
+  return api.post("/auth/login", data);
+};
+
+export const forgotPassword = (data) => {
+  return api.post("/auth/forgot-password", data);
+};
+
+export const resetPassword = (token, data) => {
+  return api.post(`/auth/reset-password/${token}`, data);
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
